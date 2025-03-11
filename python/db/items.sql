c54ebdb3f34853819459486122a93da4d4e5a803
Peach2587 <momoka.hana.1021@gmail.com>
2025-03-11 16:31:31 +0900

    [wip]STEP5 add IF NOT EXISTS

--- python/db/items.sql
+++ python/db/items.sql
@@ -1,6 +1,6 @@
-CREATE TABLE items (
+CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    category TEXT NOT NULL,
-    image_name TEXT NOT NULL
+    category TEXT,
+    image_name TEXT
 );
