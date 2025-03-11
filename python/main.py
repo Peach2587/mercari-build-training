54b170a45b06a72d5847e753fecb5e8c805e40fc
Peach2587 <momoka.hana.1021@gmail.com>
2025-03-11 14:18:21 +0900

    [wip]STEP5-1

--- python/main.py
+++ python/main.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 from contextlib import asynccontextmanager
 import json
 import hashlib
-import shutil
+import sqlite3
 
 # Define the path to the images & sqlite3 database
 images = pathlib.Path(__file__).parent.resolve() / "images"
@@ -30,7 +30,9 @@ def get_db():
 
 # STEP 5-1: set up the database connection
 def setup_database():
-    pass
+    conn = sqlite3.connect("db/mercari.sqlite3")
+    cursor = conn.cursor()
+    
 
 
 @asynccontextmanager
@@ -52,7 +54,6 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
-
 
 class HelloResponse(BaseModel):
     message: str
@@ -113,6 +114,7 @@ async def get_image(image_name):
     if not image_name.endswith(".jpg"):
         raise HTTPException(status_code=400, detail="Image path does not end with .jpg")
 
+    ## No_image のハッシュ値と一致する画像が表示されたなら、その場合も同様に以下を実行する
     if not image.exists():
         logger.debug(f"Image not found: {image}")
         image = images / "default.jpg"
@@ -124,17 +126,24 @@ class Item(BaseModel):
     name:str
     category:str
     image:str
+    db: sqlite3.Connection
 
 def insert_item(item: Item):
-    # STEP 4-1: add an implementation to store an item
-    with open('items.json') as f:
-        d_update = json.load(f)
+    # # STEP 4-1: add an implementation to store an item
+    # with open('items.json') as f:
+    #     d_update = json.load(f)
 
     d = {'name' : item.name, 'category': item.category, 'image_name':item.image}
-    d_update['items'].append(d)
+    # d_update['items'].append(d)
 
-    with open('items.json', 'w') as f:
-        json.dump(d_update, f, indent=2)
+    # with open('items.json', 'w') as f:
+    #     json.dump(d_update, f, indent=2)
+    
+    # STEP 5 : add an implementation to store an item in the database
+    cursor.execute("""
+    INSERT INTO items (name, category, image_name) 
+    VALUES (:name, :category, :image_name)
+    """, d)
 
 
 
